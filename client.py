e61ec375ffa6872d4c05a908e7276bfa4acb0355
Mayank Arora <aroram@tcd.ie>
2020-03-08 02:07:56 +0000

    Finally made something worth showing in the demo. Uses multithreading to make it asynchronus.

--- client.py
+++ client.py
@@ -1,148 +1,31 @@
 import socket
-import errno
-import time
-import sys
 import select
-import signal
+import sys
 
-# The length of the header used to send the username
-HEADER_LENGTH = 20
-
-# The number of bytes of data we can send and receive
-RECVB = 2048
-
-# The IP and port of the server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP = "127.0.0.1"
 PORT = 42069
 
-# Choose a username that reflects your personality unlike hotshot07
-my_username = input("Username: ")
+server.connect((IP, PORT))
 
-# Making a clientsocket object and binding it
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connecting it to the server
-client_socket.connect((IP, PORT))
-
-# Set blocking or non-blocking mode of the socket: if flag is 0,
-# the socket is set to non-blocking, else to blocking mode. Initially all sockets are in
-# blocking mode. In non-blocking mode, if a recv() call doesn’t find any data,
-# or if a send() call can’t immediately dispose of the data, a error exception is raised;
-# in blocking mode, the calls block until they can proceed. s.setblocking(0)
-# is equivalent to s.settimeout(0.0); s.setblocking(1) is equivalent to s.settimeout(None).
-client_socket.setblocking(False)
-
-# Function to send username to server
+sockets_list = [sys.stdin, server]
 
 
-def sendUsernameToServer(my_username):
-    username = my_username.encode('utf-8')
-    username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(username_header + username)
-
-
-# Using the above function
-sendUsernameToServer(my_username)
-
-print("Connected to remote host. You can start sending messages")
-
-
-def sendMessageToServer(message):
-    message = message.encode('utf-8')
-    message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(message_header + message)
-    return
-
-
-def receiveMessage():
-    data = client_socket.recv(HEADER_LENGTH)
-
-    if not data:
-        print('Connection closed by the server')
-        sys.exit()
-
-    messageLength, username = data.decode('utf-8').split()
-    message = client_socket.recv(int(messageLength))
-    message = message.decode('utf-8')
-    return {"Username": username, "Message": message}
-
-
+sys.stdout.write("<You>")
+sys.stdout.flush()
 while True:
+    read_sockets, write_socket, error_socket = select.select(
+        sockets_list, [], [])
 
-    # Wait for user to input a message
-    message = input(f'{my_username} > ')
-
-    # If message is not empty - send it
-    if message:
-        sendMessageToServer(message)
-
-    try:
-        # iterating over received messages, if we got any
-        while True:
-            data = receiveMessage()
-            if data:
-                print(f" {data['Username']} > {data['Message']}")
-
-    except IOError as e:
-        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
-        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
-        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
-        # If we got different error code - something happened
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Reading error: {}'.format(str(e)))
-            sys.exit()
-
-        # We just did not receive anything
-        continue
-
-
-# def sigint_handler(signum, frame):
-#     print('User interrupt. Shutting down')
-#     sys.exit()
-
-
-# signal.signal(signal.SIGINT, sigint_handler)
-
-# Socketlist over which select would iterate
-#socket_list_client = [sys.stdin, client_socket]
-
-# while True:
-#     # reading the sockets for any I/O using select library
-#     readSockets, _, exceptionSockets = select.select(
-#         socket_list_client, [], socket_list_client)
-
-#     for selectedSocket in socket_list_client:
-
-#         # If selectedSocket is the clientsocket, we have received a message
-#         if selectedSocket == client_socket:
-#             try:
-#                 data = client_socket.recv(RECVB)
-
-#                 if not data:
-#                     print("Disconnected")
-#                     sys.exit()
-
-#                 # Printing it on the terminal
-#                 else:
-#                     data = data.decode('utf-8')
-#                     print(data)
-#                     # sys.stdout.write(data)
-
-#             except Exception as e:
-#                 continue
-
-#         else:
-#             try:
-#                 # else we can send a message
-#                 # should probably use asyncio library to make it asynchronus
-#                 # Will do in next commit as herein lies the issue
-#                 message = input(f"{my_username}: ")
-#                 print(message)
-#                 sendMessageToServer(message)
-
-#                 # Encoding the message and sending it to server
-#                 message = message.encode('utf-8')
-#                 client_socket.send(message)
-
-#             except Exception as e:
-#                 continue
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print(message.decode('utf-8'))
+        else:
+            message = sys.stdin.readline()
+            message = message.encode('utf-8')
+            server.send(message)
+            sys.stdout.write("<You>")
+            sys.stdout.write(message.decode('utf-8'))
+            sys.stdout.flush()
+server.close()
--- server.py
+++ server.py
@@ -1,185 +1,74 @@
 import socket
 import select
-import signal
 import sys
+from threading import Thread
 
-# The length of the header used to get the username
-HEADER_LENGTH = 20
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# The number of bytes of data we can send and receive
-RECVB = 2048
-
-# The IP and port of the server
 IP = "127.0.0.1"
 PORT = 42069
 
-# Creating a server socket with TCP/IP and IPv4 protocols
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP, PORT))
 
-# Setting socket options
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.listen(100)
 
-# Binding the socket and IP
-server_socket.bind((IP, PORT))
+list_of_clients = []
 
-# Listening to new connections
-server_socket.listen()
-
-# Creating a list of sockets
-sockets_list = [server_socket]
-
-# Dictionary of connected clients - socket as a key, username as data
-clients = {}
-
-# List of all the usernames
-usernameList = []
-
-# Not very 'secure' at the moment. Will be in near future though
-print(f'Welcome to secure chat server. We are listening on {IP}:{PORT}...')
-
-# Handling the Ctrl+C in a cool way
+threads = []
 
 
-def sigint_handler(signum, frame):
-    print('User interrupt. Shutting down')
-    sys.exit()
+def clientthread(conn, addr):
+
+    # sends a message to the client whose user object is conn
+
+    conn.send(f"Welcome to this chatroom!".encode('utf-8'))
+
+    while True:
+        try:
+            message = conn.recv(2048)
+            message = message.decode('utf-8')
+            if message:
+                print("<" + addr[0] + "> " + message)
+
+                message_to_send = (
+                    "<" + addr[0] + "> " + message).encode('utf-8')
+                broadcast(message_to_send, conn)
+                print("here")
+
+            else:
+                remove(conn)
+
+        except:
+            continue
 
 
-signal.signal(signal.SIGINT, sigint_handler)
+def broadcast(message, connection):
+    print("Here!")
+    for clients in list_of_clients:
+        # if clients != connection:
+        try:
+            clients.send(message)
+        except:
+            clients.close()
+            remove(clients)
 
 
-# Function to get a new user
-# Format of this message
-# HEADER: (length of the username)
-# BODY: Username
-def getNewUser(client_socket):
-    try:
-        # Receiving our "header" containing message length
-        message_header = client_socket.recv(HEADER_LENGTH)
-
-        # If we received no data client has closed a connection and we can return false
-        if not len(message_header):
-            return False
-
-        # Convert header to int value
-        message_length = int(message_header.decode('utf-8').strip())
-
-        # Return an object of message header and message data
-        dataReceived = client_socket.recv(message_length)
-        dataReceived = dataReceived.decode('utf-8')
-        return {'header': message_header, 'data': dataReceived}
-
-    except:
-        return False
+def remove(connection):
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
 
 
-# Format of the message
-# HEADER: (length of the message)  username
-# BODY: message by the username
-def encodeTheMessage(selectedSocket, message):
-    username = clients[selectedSocket]
-    message_header = f"{str(len(message)) + ' '+ str(username):<{HEADER_LENGTH}}"
-    message = (message_header + message).encode('utf-8')
-    return message
-
-
-# Function to send message to all the clients, except the server
-# and the socket who sent the message
-def sendToAll(selectedSocket, message):
-
-    message = encodeTheMessage(selectedSocket, message)
-
-    for socket in sockets_list:
-        if socket != server_socket and socket != selectedSocket:
-            try:
-                socket.send(message)
-            except:
-                socket.close()
-                if socket in sockets_list:
-                    sockets_list.remove(socket)
-
-    return
-
-
-# Function to get a new message
-# Format of this message
-# HEADER: (length of the message)
-# BODY: The message
-def getMessage(selectedSocket):
-    try:
-        # Receiving our "header" containing message length
-        message_header = selectedSocket.recv(HEADER_LENGTH)
-
-        # If we received no data client has closed a connection and we can return false
-        if not len(message_header):
-            return False
-
-        # Convert header to int value
-        message_length = int(message_header.decode('utf-8').strip())
-
-        # Return an object of message header and message data
-        messageReceived = selectedSocket.recv(message_length)
-        messageReceived = messageReceived.decode('utf-8')
-        return messageReceived
-
-    except:
-        return False
-
-
-
-# Main server
 while True:
-    # Select checks for I/O in the given sockets
-    # We pass 3 parameters, the list from which we want to read
-    # the list we want to write to (empty list in this case)
-    # and an exception list
-    readSockets, _, exceptionSockets = select.select(
-        sockets_list, [], sockets_list)
+    conn, addr = server.accept()
+    list_of_clients.append(conn)
+    i = len(list_of_clients)
 
-    # iterating over the read list
-    for selectedSocket in readSockets:
+    # prints the address of the user that just connected
+    print(addr[0] + " connected")
 
-        # If it is a server socket, we have received a new connection!
-        # We call the get new user function to get the username sent by the
-        # client and store in usernameList
-
-        if selectedSocket == server_socket:
-            client_socket, client_address = server_socket.accept()
-            new_user = getNewUser(client_socket)
-
-            if new_user is False:
-                continue
-
-            sockets_list.append(client_socket)
-            username = new_user['data']
-            clients[client_socket] = username
-            usernameList.append(username)
-
-            print(f"{username} has joined the encrypted chatroom")
-
-        # Or else, someone has sent a message. We now have to get the message and
-        # send it to all the clients on the chatlist
-
-        else:
-
-            try:
-                # Try getting the message
-                message = getMessage(selectedSocket)
-
-                # If we get the message, send to all the clients
-                if message:
-                    sendToAll(selectedSocket, message)
-
-                else:
-                    # If we didn't receive any data, connection has been closed by the client
-                    print('Closed connection from: {}'.format(
-                        clients[selectedSocket]))
-
-                    sockets_list.remove(selectedSocket)
-
-                    del clients[selectedSocket]
-
-                    continue
-
-            except:
-                continue
+    # creates and individual thread for every user
+    # that connects
+    process = Thread(target=clientthread, args=[conn, addr])
+    process.start()
+    threads.append(process)
