125d482927daecbeca28c1f75b6e87f201f46569
Mayank Arora <aroram@tcd.ie>
2020-03-08 03:28:26 +0000

    added username functionality

--- client.py
+++ client.py
@@ -2,30 +2,44 @@ import socket
 import select
 import sys
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP = "127.0.0.1"
 PORT = 42069
-
-server.connect((IP, PORT))
-
-sockets_list = [sys.stdin, server]
+HEADER_LENGTH = 10
 
 
-sys.stdout.write("<You>")
-sys.stdout.flush()
+client_socket.connect((IP, PORT))
+# client_socket.setblocking(False)
+my_username = input("Username: ")
+
+
+def sendUsernameToServer(my_username):
+    username = my_username.encode('utf-8')
+    username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+    client_socket.send(username_header + username)
+
+
+# Using the above function
+sendUsernameToServer(my_username)
+
+sockets_list = [sys.stdin, client_socket]
+
+
 while True:
     read_sockets, write_socket, error_socket = select.select(
         sockets_list, [], [])
 
     for socks in read_sockets:
-        if socks == server:
+        if socks == client_socket:
             message = socks.recv(2048)
             print(message.decode('utf-8'))
         else:
             message = sys.stdin.readline()
             message = message.encode('utf-8')
-            server.send(message)
-            sys.stdout.write("<You>")
+            client_socket.send(message)
+            sys.stdout.write(str(my_username) + " > ")
             sys.stdout.write(message.decode('utf-8'))
             sys.stdout.flush()
-server.close()
+
+
+client_socket.close()
--- server.py
+++ server.py
@@ -3,72 +3,100 @@ import select
 import sys
 from threading import Thread
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 IP = "127.0.0.1"
 PORT = 42069
 
-server.bind((IP, PORT))
+HEADER_LENGTH = 10
 
-server.listen(100)
+server_socket.bind((IP, PORT))
 
-list_of_clients = []
+server_socket.listen(100)
+
+clients = {}
+
+clientList = []
 
 threads = []
 
 
-def clientthread(conn, addr):
+def getNewUser(client_socket):
+    try:
+        # Receiving our "header" containing message length
+        message_header = client_socket.recv(HEADER_LENGTH)
 
-    # sends a message to the client whose user object is conn
+        # If we received no data client has closed a connection and we can return false
+        if not len(message_header):
+            return False
 
-    conn.send(f"Welcome to this chatroom!".encode('utf-8'))
+        # Convert header to int value
+        message_length = int(message_header.decode('utf-8').strip())
+
+        # Return an object of message header and message data
+        dataReceived = client_socket.recv(message_length)
+        dataReceived = dataReceived.decode('utf-8')
+        return {'header': message_header, 'data': dataReceived}
+
+    except:
+        return False
+
+
+def clientthread(client_socket, client_address):
+
+    new_user = getNewUser(client_socket)
+
+    username = new_user['data']
+
+    clients[client_socket] = username
+
+    print(f"{username} has joined the encrypted chatroom")
+
+    username = clients[client_socket]
+    # sends a message to the client whose user object is client_socket
+    client_socket.send(
+        f"Welcome to this secure chatroom {username}!".encode('utf-8'))
 
     while True:
         try:
-            message = conn.recv(2048)
+            message = client_socket.recv(2048)
             message = message.decode('utf-8')
             if message:
-                print("<" + addr[0] + "> " + message)
-
-                message_to_send = (
-                    "<" + addr[0] + "> " + message).encode('utf-8')
-                broadcast(message_to_send, conn)
-                print("here")
+                print(username + " > " + message)
+                message_to_send = (username + " > " + message).encode('utf-8')
+                sendToAll(message_to_send, client_socket)
 
             else:
-                remove(conn)
+                remove(client_socket)
 
         except:
             continue
 
 
-def broadcast(message, connection):
-    print("Here!")
-    for clients in list_of_clients:
-        # if clients != connection:
-        try:
-            clients.send(message)
-        except:
-            clients.close()
-            remove(clients)
+def sendToAll(message, connection):
+
+    for clients in clientList:
+        if clients != connection:
+            try:
+                clients.send(message)
+            except Exception as e:
+                remove(clients)
 
 
 def remove(connection):
-    if connection in list_of_clients:
-        list_of_clients.remove(connection)
+    if connection in clientList:
+        clientList.remove(connection)
 
 
+
+# Main Server
 while True:
-    conn, addr = server.accept()
-    list_of_clients.append(conn)
-    i = len(list_of_clients)
+    client_socket, client_address = server_socket.accept()
 
-    # prints the address of the user that just connected
-    print(addr[0] + " connected")
+    clientList.append(client_socket)
 
-    # creates and individual thread for every user
-    # that connects
-    process = Thread(target=clientthread, args=[conn, addr])
+    # Creates and individual thread for every user
+    process = Thread(target=clientthread, args=[client_socket, client_address])
     process.start()
     threads.append(process)
